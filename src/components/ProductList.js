ac08ea48d94f9102da1c8adefa03fa24a2497725
Ramanand Dubey <167943838+rama-69er@users.noreply.github.com>
2025-02-14 12:50:10 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,9 +3,9 @@ import '../css/productList.css';
 
 function ProductList({ addToCart }) {
     const products = [
-        { id: 1, name: 'Product 1', description: 'This is a description of Product 1', price: 9.99 },
-        { id: 2, name: 'Product 2', description: 'This is a description of Product 2', price: 19.99 },
-        { id: 3, name: 'Product 3', description: 'This is a description of Product 3', price: 29.99 },
+        { id: 1, name: '1', description: 'This is a description of Product 1', price: 9.99 },
+        { id: 2, name: '2', description: 'This is a description of Product 2', price: 19.99 },
+        { id: 3, name: '3', description: 'This is a description of Product 3', price: 29.99 },
         // Add more products as needed...
     ];
 
